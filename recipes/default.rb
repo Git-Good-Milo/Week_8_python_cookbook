c2ab009ff9e38c9d6907878d73f1ea41d67728e9
Miles Eastwood <MEastwood@spartaglobal.com>
2019-10-31 09:41:23 +0000

    Inital commit. Python and pip installed and working as confirmed by unit and integration tests

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,15 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+apt_update 'update_sources' do
+  action :update
+end
+
+package "python" do
+  action :install
+end
+
+package "python-pip" do
+  action :install
+end
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,23 +7,26 @@
 require 'spec_helper'
 
 describe 'python_cookbook::default' do
-  context 'When all attributes are default, on Ubuntu 18.04' do
+  context 'When all attributes are default, on Ubuntu 16.04' do
     # for a complete list of available platforms and versions see:
     # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'ubuntu', '18.04'
+    platform 'ubuntu', '16.04'
+
+    it "should check/get updates" do
+      expect(chef_run).to update_apt_update "update_sources"
+    end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
 
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it "should install python from a package" do
+      expect(chef_run).to install_package "python"
     end
+
+    it "should install pip from a package" do
+      expect(chef_run).to install_package "python-pip"
+    end
+
   end
 end
