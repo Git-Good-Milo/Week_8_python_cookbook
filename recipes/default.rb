663b9f171587a21fbca181d7427ee16abf7013c3
Miles Eastwood <MEastwood@spartaglobal.com>
2019-10-31 11:27:36 +0000

    Testing commit2

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,9 +9,15 @@ apt_update 'update_sources' do
 end
 
 package "python" do
+  options '--allow-unauthenticated'
   action :install
 end
 
 package "python-pip" do
+  options '--allow-unauthenticated'
   action :install
+end
+
+execute 'install requests==2.3.0' do
+  command 'pip-install requests==2.3.0'
 end
